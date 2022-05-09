f0ee70344d6f034f741ac09be706b38a09361a43
Akash Pal <creativeakashpal@gmail.com>
2022-05-09 21:46:26 +0530

    added comment and removed game function

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// random coputer choices 
 function computerPlay() {
+    // gives a number between 0-3
     computerChoice = Math.floor(Math.random() * 3);
+    // to hold computer's choice (rps)
     let rps = "";
 
+    // assign 0, 1 and 2 with the value of rock, paper and scissor respectively
     switch (computerChoice) {
         case 0: rps = "rock";
             break;
@@ -13,75 +17,66 @@ function computerPlay() {
     return rps;
 }
 
+// decide winner of one round
 function playRound(computerSelection, playerSelection) {
+    // store the winning result
     let str = ''
+    // tie condition
     if (computerSelection === playerSelection) {
         str = "It's a Tie!!";
-        alert(str);
     }
-    else if (computerSelection === 'rock' && playerSelection === 'paper') {
-        str = 'You Win!! Paper beats Rock';
-        alert(str);
+    // winning condition
+    else if (computerSelection === 'rock' && playerSelection === 'paper'
+        || computerSelection === 'paper' && playerSelection === 'scissor'
+        || computerSelection === 'scissor' && playerSelection === 'rock') {
+        str = 'You Win!!';
     }
-    else if (computerSelection === 'rock' && playerSelection === 'scissor') {
-        str = 'You Lose!! Rock beats Scissor';
-        alert(str);
+    // losing condition
+    else if (computerSelection === 'rock' && playerSelection === 'scissor'
+        || computerSelection === 'paper' && playerSelection === 'rock'
+        || computerSelection === 'scissor' && playerSelection === 'paper') {
+        str = 'You Lose!!';
     }
-    else if (computerSelection === 'paper' && playerSelection === 'rock') {
-        str = 'You Lose!! Paper beats Rock';
-        alert(str);
-    }
-    else if (computerSelection === 'paper' && playerSelection === 'scissor') {
-        str = 'You Win!! Scissor beats Paper';
-        alert(str);
-    }
-    else if (computerSelection === 'scissor' && playerSelection === 'paper') {
-        str = 'You Lose!! Scissor beats Paper';
-        alert(str);
-    }
-    else if (computerSelection === 'scissor' && playerSelection === 'rock') {
-        str = 'You Win!! Rock beats Scissor';
-        alert(str);
-    }
+
     return str;
 }
 
-function game() {
+// function game() {
 
-    let playerCount = 0,
-        compCount = 0;
+//     let playerCount = 0,
+//         compCount = 0;
 
-    for (let i = 0; i < 5; i++) {
-        let regex = /Win/;
-        let computerSelection = computerPlay();
-        let playerSelection = prompt('Choose Rock Paper or Scissor').toLowerCase();
-        let winStr = playRound(computerSelection, playerSelection);
+//     for (let i = 0; i < 5; i++) {
+//         let regex = /Win/;
+//         let computerSelection = computerPlay();
+//         let playerSelection = prompt('Choose Rock Paper or Scissor').toLowerCase();
+//         let winStr = playRound(computerSelection, playerSelection);
 
-        if (/Tie/.test(winStr)) {
-            playerCount++;
-            compCount++;
-        }
-        else if (regex.test(winStr)) {
-            playerCount++;
-        }
-        else {
-            compCount++;
-        }
-        console.log(`your choice: ${playerSelection}  computer choice: ${computerSelection}`);
-        console.log("computer :" + compCount + " player :" + playerCount);
-    }
-    if (compCount > playerCount) {
-        console.log("You Lose!!");
-    }
-    else if (compCount === playerCount) {
-        console.log("It's a Tie!!");
-    }
-    else {
-        console.log("You Win!!");
-    }
-}
+//         if (/Tie/.test(winStr)) {
+//             playerCount++;
+//             compCount++;
+//         }
+//         else if (regex.test(winStr)) {
+//             playerCount++;
+//         }
+//         else {
+//             compCount++;
+//         }
+//         console.log(`your choice: ${playerSelection}  computer choice: ${computerSelection}`);
+//         console.log("computer :" + compCount + " player :" + playerCount);
+//     }
+//     if (compCount > playerCount) {
+//         console.log("You Lose!!");
+//     }
+//     else if (compCount === playerCount) {
+//         console.log("It's a Tie!!");
+//     }
+//     else {
+//         console.log("You Win!!");
+//     }
+// }
 
 
-game();
+// game();
 
 
