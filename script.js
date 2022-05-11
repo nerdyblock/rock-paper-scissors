8ced2cc6972bb02a8205176aeb9104e0faa14e0d
Akash Pal <creativeakashpal@gmail.com>
2022-05-11 19:08:02 +0530

    made the game working

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// for player score
+let playerCount = 0;
+// for computer score
+let computerCount = 0;
+// for computer choice
+let computerSelection = computerPlay();
+// for player choice
+let playerSelection;
+// span to show player score
+let playerScore = document.querySelector('#player-score');
+// span to show computer score
+let computerScore = document.querySelector('#computer-score');
+// result of one round in text form
+let resultText = document.querySelector('.result>h3');
+// to show final result on screen through text
+let finalResultText = document.querySelector('#final-result-text');
+// div to play again
+let playAgain = document.querySelector('.play-again');
+
 // random coputer choices 
 function computerPlay() {
     // gives a number between 0-3
@@ -17,66 +36,76 @@ function computerPlay() {
     return rps;
 }
 
+// nodelist of class choice (div container for rps)
+let choice = document.querySelectorAll('.choice');
+choice.forEach(item => item.addEventListener('click', playGame));
+
+function playGame() {
+    // id of the cliked choice div
+    playerSelection = this.id;
+    // play one round
+    playRound(computerSelection, playerSelection);
+    // check if condition to end game satisfy
+    endGame(computerCount, playerCount);
+    // reset the game if it has ended
+    resetGame();
+}
+
 // decide winner of one round
 function playRound(computerSelection, playerSelection) {
-    // store the winning result
-    let str = ''
     // tie condition
     if (computerSelection === playerSelection) {
-        str = "It's a Tie!!";
+        resultText.textContent = `It's a Tie!! You both chose ${playerSelection}`;
     }
     // winning condition
     else if (computerSelection === 'rock' && playerSelection === 'paper'
         || computerSelection === 'paper' && playerSelection === 'scissor'
         || computerSelection === 'scissor' && playerSelection === 'rock') {
-        str = 'You Win!!';
+        // show the winning result on screen
+        resultText.textContent = `Impressive YOU WIN!! Your ${playerSelection} beat computer's ${computerSelection}`;
+        // increase the player score count
+        playerCount++;
+        // show the player score on screen
+        playerScore.textContent = playerCount;
     }
     // losing condition
     else if (computerSelection === 'rock' && playerSelection === 'scissor'
         || computerSelection === 'paper' && playerSelection === 'rock'
         || computerSelection === 'scissor' && playerSelection === 'paper') {
-        str = 'You Lose!!';
+        // show the losing result on screen
+        resultText.textContent = `Too bad YOU LOST :( Computer's ${computerSelection} beat your ${playerSelection}`;
+        // increase the computer score count
+        computerCount++;
+        // show the computer score on screen
+        computerScore.textContent = computerCount;
     }
 
-    return str;
+    // return the player score and computer score after one round
+    return [computerCount, playerCount];
 }
 
-// function game() {
+function endGame(computerCount, playerCount){
+    // function will only run if either one reaches the score of 5
+    if(computerCount === 5 || playerCount === 5) {
+        // show final result on screen won/lost
+        if(playerCount > computerCount) {
+            finalResultText.textContent = 'WOW Impressive You Won!!'
+        } 
+        else {
+            finalResultText.textContent = 'Too Bad YOU LOST :('
+        }
+        // remove hover effect and playgame function form choice div so that it is not playable after the game ends to play again press the button
+        choice.forEach(item => {
+            item.classList.remove('choice-transform');
+            item.removeEventListener('click', playGame);
+        });
+        // make the play again button visible 
+        playAgain.style.display = 'block';
+    }
+} 
 
-//     let playerCount = 0,
-//         compCount = 0;
-
-//     for (let i = 0; i < 5; i++) {
-//         let regex = /Win/;
-//         let computerSelection = computerPlay();
-//         let playerSelection = prompt('Choose Rock Paper or Scissor').toLowerCase();
-//         let winStr = playRound(computerSelection, playerSelection);
-
-//         if (/Tie/.test(winStr)) {
-//             playerCount++;
-//             compCount++;
-//         }
-//         else if (regex.test(winStr)) {
-//             playerCount++;
-//         }
-//         else {
-//             compCount++;
-//         }
-//         console.log(`your choice: ${playerSelection}  computer choice: ${computerSelection}`);
-//         console.log("computer :" + compCount + " player :" + playerCount);
-//     }
-//     if (compCount > playerCount) {
-//         console.log("You Lose!!");
-//     }
-//     else if (compCount === playerCount) {
-//         console.log("It's a Tie!!");
-//     }
-//     else {
-//         console.log("You Win!!");
-//     }
-// }
-
-
-// game();
-
+function resetGame() {
+    // reload the screen if play again button is clicked
+    playAgain.addEventListener('click', () => window.location.reload());
+}
 
