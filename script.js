81d50e671ceee13fde4e7f57d726085e80c9b4b8
Akash Pal <creativeakashpal@gmail.com>
2022-05-12 11:38:06 +0530

    made comment changes

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
-// for player score
 let playerCount = 0;
-// for computer score
 let computerCount = 0;
-// for computer choice
 let computerSelection = computerPlay();
-// for player choice
 let playerSelection;
-// span to show player score
 let playerScore = document.querySelector('#player-score');
-// span to show computer score
 let computerScore = document.querySelector('#computer-score');
-// result of one round in text form
 let resultText = document.querySelector('.result>h3');
-// to show final result on screen through text
 let finalResultText = document.querySelector('#final-result-text');
-// div to play again
 let playAgain = document.querySelector('.play-again');
 
 // random coputer choices 
 function computerPlay() {
     // gives a number between 0-3
     computerChoice = Math.floor(Math.random() * 3);
-    // to hold computer's choice (rps)
     let rps = "";
 
     // assign 0, 1 and 2 with the value of rock, paper and scissor respectively
@@ -43,11 +33,9 @@ choice.forEach(item => item.addEventListener('click', playGame));
 function playGame() {
     // id of the cliked choice div
     playerSelection = this.id;
-    // play one round
     playRound(computerSelection, playerSelection);
     // check if condition to end game satisfy
     endGame(computerCount, playerCount);
-    // reset the game if it has ended
     resetGame();
 }
 
@@ -63,7 +51,6 @@ function playRound(computerSelection, playerSelection) {
         || computerSelection === 'scissor' && playerSelection === 'rock') {
         // show the winning result on screen
         resultText.textContent = `Impressive YOU WIN!! Your ${playerSelection} beat computer's ${computerSelection}`;
-        // increase the player score count
         playerCount++;
         // show the player score on screen
         playerScore.textContent = playerCount;
@@ -74,25 +61,24 @@ function playRound(computerSelection, playerSelection) {
         || computerSelection === 'scissor' && playerSelection === 'paper') {
         // show the losing result on screen
         resultText.textContent = `Too bad YOU LOST :( Computer's ${computerSelection} beat your ${playerSelection}`;
-        // increase the computer score count
         computerCount++;
         // show the computer score on screen
         computerScore.textContent = computerCount;
     }
 
-    // return the player score and computer score after one round
     return [computerCount, playerCount];
 }
 
 function endGame(computerCount, playerCount){
-    // function will only run if either one reaches the score of 5
     if(computerCount === 5 || playerCount === 5) {
         // show final result on screen won/lost
         if(playerCount > computerCount) {
             finalResultText.textContent = 'WOW Impressive You Won!!'
+            finalResultText.style.fontSize = '18px';
         } 
         else {
             finalResultText.textContent = 'Too Bad YOU LOST :('
+            finalResultText.style.fontSize = '18px';
         }
         // remove hover effect and playgame function form choice div so that it is not playable after the game ends to play again press the button
         choice.forEach(item => {
